a221c041c9c0d44837204bae425eefa0ae5688ca
Zeha Irawan <jangkaringbumi@gmail.com>
2020-08-11 19:23:03 +0700

    create common folder & Loading

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import Auth from './screens/Auth';
+import LoggedIn from './screens/LoggedIn';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      jwt: '',
+    };
+  }
+
+  render() {
+    if (!this.state.jwt) {
+      return <Auth />;
+    } else if (this.state.jwt) {
+      return <LoggedIn />;
+    }
+  }
+}
---
+++ src/screens/LoggedIn.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export const LoggedIn = () => {
+  return <Text>Here By dragon</Text>;
+};
