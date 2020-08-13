d63ef5ad3ec3bf9cde9a059f3fe569cb7694bfd3
Zeha Irawan <jangkaringbumi@gmail.com>
2020-08-14 06:08:39 +0700

    refactor stack navigator

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import {Button, Text, View} from 'react-native';
+import ProductList from '../components/ProductList';
 
 const HomeScreen = () => {
   return (
     <View>
       <Text>Signed in!</Text>
       <Button title="Sign out" onPress={() => console.log('Logout')} />
+      <ProductList />
     </View>
   );
 };
